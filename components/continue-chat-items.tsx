de794b96a2067258ac59e20d6999351edbc769bf
Miles Jaffee <mej327@lehigh.edu>
2025-08-27 17:42:21 -0400

    worked on continuing chat with list of preexisting chats

---
+++ components/continue-chat-items.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Chat } from '@/lib/types'
+import { AnimatePresence, motion } from 'framer-motion'
+
+import { removeChat, shareChat } from '@/app/actions'
+
+import { SidebarActions } from '@/components/sidebar-actions'
+import { SidebarItem } from '@/components/sidebar-item'
+
+interface SidebarItemsProps {
+  chats?: Chat[]
+}
+
+export function ContinueChatItems({ chats }: SidebarItemsProps) {
+  if (!chats?.length) return null
+
+  return (
+    <AnimatePresence>
+      {chats.map(
+        (chat, index) =>
+          chat && (
+            <motion.div
+              key={chat?.id}
+              exit={{
+                opacity: 0,
+                height: 0
+              }}
+            >
+              <ContinueChatItem index={index} chat={chat}>
+              </ContinueChatItem>
+            </motion.div>
+          )
+      )}
+    </AnimatePresence>
+  )
+}
--- components/continue-chat.tsx
+++ components/continue-chat.tsx
@@ -1,7 +1,5 @@
-import { clearChats, getChats } from '@/app/actions'
-import { ClearHistory } from '@/components/clear-history'
+import { getChats } from '@/app/actions'
 import { SidebarItems } from '@/components/sidebar-items'
-import { ThemeToggle } from '@/components/theme-toggle'
 import { cache } from 'react'
 
 interface SidebarListProps {
@@ -13,7 +11,7 @@ const loadChats = cache(async (userId?: string) => {
   return await getChats(userId)
 })
 
-export async function SidebarList({ userId }: SidebarListProps) {
+export async function ContinueChat({ userId }: SidebarListProps) {
   const chats = await loadChats(userId) //this is the key!!
 
   return (
@@ -21,17 +19,13 @@ export async function SidebarList({ userId }: SidebarListProps) {
       <div className="flex-1 overflow-auto">
         {chats?.length ? (
           <div className="space-y-2 px-2">
-            <SidebarItems chats={chats} />
+            <ContinueChatItems chats={chats} />
           </div>
         ) : (
           <div className="p-8 text-center">
             <p className="text-sm text-muted-foreground">No chat history</p>
           </div>
         )}
-      </div>
-      <div className="flex items-center justify-between p-4">
-        <ThemeToggle />
-        <ClearHistory clearChats={clearChats} isEnabled={chats?.length > 0} />
       </div>
     </div>
   )
